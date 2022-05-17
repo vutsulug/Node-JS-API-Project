e3a285a66981bc451da9b9b3c0eaa54c9c449895
vutsulug <nyikovutsulu@gmail.com>
2022-05-17 14:06:56 +0200

    added mysql and cors and made changed on the server file

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("Hi");
+//
+
+var express = require('express');
+var cors = require('cors');
+var app = express();
+
+//CORS - Cross-Origin Resource Sharing. it allows us to relax the security
+//It is applied to an API
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+console.log('Project running!');
